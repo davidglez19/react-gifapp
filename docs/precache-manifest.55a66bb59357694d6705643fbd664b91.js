1c9034b631905010011cd2b148619caec90ca828
Amilcar <davidglez.ucol@gmail.com>
2020-06-26 16:10:42 -0500

    docs agregado

---
+++ docs/precache-manifest.55a66bb59357694d6705643fbd664b91.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b90a09ac68a4246321cefbfc49550eb8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2d4cdc8947da749c9d58",
+    "url": "/static/css/main.5db19ef8.chunk.css"
+  },
+  {
+    "revision": "51e34cff51b285543b30",
+    "url": "/static/js/2.04409896.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.04409896.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d4cdc8947da749c9d58",
+    "url": "/static/js/main.f85180bf.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
